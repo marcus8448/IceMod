e5ea9d0712942ba736914dec50f9f6b4e8a85851
marcus8448 <marcus2475@outlook.com>
2018-09-30 20:04:07 -0700

    What was I thinking...

--- iceMod/IceMod.java
+++ iceMod/IceMod.java
@@ -35,144 +35,97 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 
-/* 	MOD INFO */
 	@Mod( modid = "IceMod", name = "Ice Mod", version = "1.5")
 	@NetworkMod(clientSideRequired=true, serverSideRequired=true)	
 
 
 public class IceMod {
 
-/*	PROXY INFO */
 	@SidedProxy(clientSide = "iceMod.proxies.ClientProxy", serverSide = "iceMod.proxies.CommonProxy")
 	public static CommonProxy proxy;
 		
 
-//  Declare the IceMod Creative Tab
         public static CreativeTabs IceModTab;
-//  Declare the Frozen Stick
-		public static Item FrozenStick;
-    
-//  Declare the IceMaterial
+	
         public static EnumToolMaterial IceMaterial = EnumHelper.addToolMaterial("IceMaterial", 4, 1700, 9.0F, 4.0F, 11);
-        
-//  Declare the IceSword
-        public static Item IceSword;
-        
-//  Declare the IcePickaxe
-        public static Item IcePick;
-        
-//  Declare the IceFragment
-        public static Item IceFragment;
-        
-//  Declare the IcyOre
+	
+        public static BiomeGenBase IceLands; 
+	
         public static Block IcyOre;
-        
-//  Declare the FrozenBeef
-        public static Item FrozenBeef;
-
-//  Declare the Icy cold biome
-        public static  BiomeGenBase IceLands;  
-          
-//  Declare the Flint and Diamond
+	
+	public static Item FrozenStick;
+	public static Item IceSword;
+        public static Item IcePick;
+        public static Item IceFragment;
+        public static Item FrozenBeef; 
         public static Item FlintAndDiamond;
-
-//  Declare the Hunk O' Ice
         public static Item HunkOIce;
         
-
-
-
-
-/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */	
-
-// Loading everything!
-@EventHandler	
-	public  void preInit( FMLPreInitializationEvent event ) 
+	@EventHandler	
+	public void preInit(FMLPreInitializationEvent event) 
 	{
 
-//  Load the IceMod Creative Tab
          IceModTab = new CreativeTabs("IceMod") {
         public ItemStack getIconItemStack() 
-		return new ItemStack(FrozenBeef, 1, 0);   // Icon, Stack Size, Tab Position
-            }
-        };
+		return new ItemStack(FrozenBeef, 1, 0);
+	 }};
         
-//  Load the FrozenStick
-         FrozenStick = new FrozenStick(999, "FrozenStick").setCreativeTab(CreativeTabs.tabMisc);
+        FrozenStick = new FrozenStick(999, "FrozenStick").setCreativeTab(CreativeTabs.tabMisc);
         GameRegistry.registerItem(FrozenStick, "FrozenStick");
-        LanguageRegistry.addName(FrozenStick, "§9Frozen Stick");     
-        
-//  Load the IceSword
+        LanguageRegistry.addName(FrozenStick, "§9Frozen Stick");
+
         IceSword = new IceSword(1000, EnumToolMaterial.EMERALD, "IceSword").setCreativeTab(CreativeTabs.tabCombat);
         GameRegistry.registerItem(IceSword, "IceSword");
         LanguageRegistry.addName(IceSword, "§9Ice Sword");     
         
-//  Load the IcePickaxe
         IcePick = new IcePickaxe(1001, IceMod.IceMaterial, "IcePickaxe").setCreativeTab(CreativeTabs.tabTools);
         GameRegistry.registerItem(IcePick, "IcePickaxe");
         LanguageRegistry.addName(IcePick, "§9Ice Pickaxe");  	
 		
-//  Load the IceFragment
         IceFragment = new IceFragment(1002, "IceFragment").setCreativeTab(CreativeTabs.tabMisc);
         GameRegistry.registerItem(IceFragment, "IceFragment");
         LanguageRegistry.addName(IceFragment, "§9Ice Fragment");        
         
-//  Load the IcyOre
         IcyOre = new IcyOre(1003, Material.rock, "IcyiOre").setCreativeTab(CreativeTabs.tabMaterials);
         GameRegistry.registerBlock(IcyOre, "IcyOre");
         LanguageRegistry.addName(IcyOre, "§9Icy Ore"); 
 		MinecraftForge.setBlockHarvestLevel(IcyOre, "pickaxe", 3);
 
 		
-//  Load the FrozenBeef
         FrozenBeef = new FrozenBeef(1004, 3, 3.0F, true, "FrozenBeef").setCreativeTab(CreativeTabs.tabFood);
         GameRegistry.registerItem(FrozenBeef, "FrozenBeef");
         LanguageRegistry.addName(FrozenBeef, "§9Frozen Beef"); 
         
-//  Load the Ice Lands biome
         IceLands = new IceLands(54);
         GameRegistry.addBiome(IceLands);        
 
-//  Load the Flint and diamond
         FlintAndDiamond = new FlintAndIce(1006, "FlintAndDiamond").setCreativeTab(CreativeTabs.tabTools);
         GameRegistry.registerItem(FlintAndDiamond, "FlintAndDiamond");
         LanguageRegistry.addName(FlintAndDiamond, "§9Flint & Diamond");
         
-//  Load the Hunk O' Ice
-         HunkOIce = new HunkOIce(1007, "HunkOIce").setCreativeTab(CreativeTabs.tabMisc);
+        HunkOIce = new HunkOIce(1007, "HunkOIce").setCreativeTab(CreativeTabs.tabMisc);
         GameRegistry.registerItem(HunkOIce, "Hunk O Ice");
         LanguageRegistry.addName(HunkOIce, "Hunk O Ice"); 
         EntityRegistry.registerGlobalEntityID(EntityHunkOIce.class, "Hunk O Ice", 1);
-         EntityRegistry.registerModEntity(EntityHunkOIce.class, "Hunk O Ice", 1, this, 256, 1, true);
-         LanguageRegistry.instance().addStringLocalization("entity.modid.Hunk O Ice.name", "Hunk O Ice");
-         RenderingRegistry.registerEntityRenderingHandler(EntityHunkOIce.class, new RenderHunkOIce(HunkOIce));
-   
-    
-                  
-/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */	
-
+        EntityRegistry.registerModEntity(EntityHunkOIce.class, "Hunk O Ice", 1, this, 256, 1, true);
+        LanguageRegistry.instance().addStringLocalization("entity.modid.Hunk O Ice.name", "Hunk O Ice");
+        RenderingRegistry.registerEntityRenderingHandler(EntityHunkOIce.class, new RenderHunkOIce(HunkOIce));
 	}
 
 @EventHandler
 	public static void init( FMLInitializationEvent event ) 
 	{
-	
-/**	
- * RECIPES SECTION 
- * *********************************************************** */
-
-// The Recipe for the Ice Sword
-        GameRegistry.addRecipe(new ItemStack(IceSword, 1), new Object[]
-        {
+		
+		GameRegistry.addRecipe(new ItemStack(IceSword, 1), new Object[]
+    	    {
                 "IDI",
                 "IDI",
                 " S ",
             'D', Item.diamond,
             'S', IceMod.FrozenStick,
-			'I', Block.ice,
+            'I', Block.ice,
         });
 
- // The Recipe for the Ice Pickaxe
         GameRegistry.addRecipe(new ItemStack(IcePick, 1), new Object[]
         {
                 "IXI",
@@ -182,11 +135,9 @@ public class IceMod {
             'X', Item.diamond,
 			'I', Block.Ice,
         }); 
-        
- // The Smelting Recipe for ICYORE
-        GameRegistry.addSmelting(IcyOre.blockID, (new ItemStack(Ice, 9)), 100);
 
-// The Recipe for Frozen Beef         
+        GameRegistry.addSmelting(IcyOre.blockID, (new ItemStack(Ice, 9)), 100);
+        
         GameRegistry.addRecipe(new ItemStack(FrozenBeef, 1), new Object[]
         {
                 "CSC",
@@ -196,11 +147,9 @@ public class IceMod {
             'B', Item.beefCooked,
 			's', IceMod.IceFragment,
         });
-		
-// The Smelting Recipe for Frozen Beef
-        GameRegistry.addSmelting(FrozenBeef, (new ItemStack(Beef, 1)), new Object []
 
-// The Recipe for the Flint And Diamond      
+        GameRegistry.addSmelting(FrozenBeef, (new ItemStack(Beef, 1)), new Object []
+  
         GameRegistry.addRecipe(new ItemStack(FlintAndDiamond, 1), 10);
         {
                 "   ",
@@ -209,7 +158,7 @@ public class IceMod {
             'I', Block.ice,
             'F', Item.flint,
         });
-        // The Recipe for the Ice Fragment   
+
         GameRegistry.addRecipe(new ItemStack(IceFragment, 9), new Object[]
         {
                 "   ",
@@ -217,7 +166,7 @@ public class IceMod {
                 "   ",
             'I', Block.ice,
         });
-        // The Recipe for the Frozen Sticks    
+
         GameRegistry.addRecipe(new ItemStack(FrozenStick, 1), new Object[]
         {
                 "III",
@@ -226,24 +175,11 @@ public class IceMod {
             'I', IceMod.IceFragment,
             'S', Item.stick,
         });
+
+ 	   GameRegistry.registerWorldGenerator(new IcyOreGen());
+   	 LanguageRegistry.instance().addStringLocalization("itemGroup.IceModTab", "en_US", "§9Ice Mod");  
+	LanguageRegistry.instance().addStringLocalization("itemGroup.IceModTab", "en_CA", "§9Ice Mod");  
         
-  
-
-/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */
-
-
-// Register Icy Ore Generation
-    GameRegistry.registerWorldGenerator(new IcyOreGen());
-
-// Change The IceMod Tab's Name
-        LanguageRegistry.instance().addStringLocalization("itemGroup.IceModTab", "en_US", "§9Ice Mod");  
-		LanguageRegistry.instance().addStringLocalization("itemGroup.IceModTab", "en_CA", "§9Ice Mod");  
-        
-/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */	
-
-	
-	}
-
 @EventHandler
 	public static void postInit( FMLPostInitializationEvent event ) 
 	{
